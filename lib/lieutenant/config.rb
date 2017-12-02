2ef3be3b1a128208a2fbc0dcc909ae9807c794a7
Gabriel Teles <gab.teles@hotmail.com>
2017-12-02 21:43:51 -0200

    refact(config) removing delegation and magic methods

--- lib/lieutenant/config.rb
+++ lib/lieutenant/config.rb
@@ -3,35 +3,24 @@
 module Lieutenant
   # Manages configuration
   class Config
-    extend Forwardable
-
-    def initialize
-      event_bus   = EventBus::InMemory.new
-      event_store = EventStore.new(EventStore::InMemory.new, event_bus)
-      aggregate_repository = AggregateRepository.new(event_store)
-      command_sender = CommandSender.new(aggregate_repository)
-
-      @registrations = {
-        event_bus: event_bus,
-        event_store: event_store,
-        aggregate_repository: aggregate_repository,
-        command_sender: command_sender
-      }
+    # :reek:BooleanParameter
+    def event_bus(implementation = false)
+      return @event_bus = implementation if implementation
+      @event_bus ||= EventBus::InMemory.new
     end
 
-    def respond_to_missing?(name, *)
-      registration_method?(name) || super
+    # :reek:BooleanParameter
+    def event_store(implementation = false)
+      return @event_store_implementation = implementation if implementation
+      @event_store ||= EventStore.new(@event_store_implementation, event_bus)
     end
 
-    def method_missing(name, *args, &block)
-      return super unless registration_method?(name)
-      args.empty? ? get(name) : set(name, *args)
+    def aggregate_repository
+      @aggregate_repository ||= AggregateRepository.new(event_store)
     end
 
-    private # rubocop:disable Lint/UselessAccessModifier
-
-    def_delegator :@registrations, :[], :get
-    def_delegator :@registrations, :[]=, :set
-    def_delegator :@registrations, :key?, :registration_method?
+    def command_sender
+      @command_sender ||= CommandSender.new(aggregate_repository)
+    end
   end
 end
